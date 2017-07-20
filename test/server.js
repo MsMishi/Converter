c31b9a0c2a044f961af866738fcf02541f7c5c6d
MsMishi <mcwcharles@gmail.com>
2017-07-20 08:55:15 -0700

    Fin

--- test/server.js
+++ test/server.js
@@ -10,14 +10,14 @@ describe('Color Code Converter API', function() {
     it('returns status 200', function() {
       request(url, function(error, response, body) {
         expect(response.statusCode).to.equal(200)
-        done()
+        
       })
     })
 
     it('returns the color in hex', function() {
       request(url, function(error, response, body) {
         expect(body).to.equal('ffffff')
-        done()
+        
       })
     })
   })
@@ -29,14 +29,14 @@ describe('Color Code Converter API', function() {
     it('returns status 200', function() {
       request(url, function(error, response, body) {
         expect(response.statusCode).to.equal(200)
-        done()
+        
       })
     })
 
     it('returns the color in RGB', function() {
       request(url, function(error, response, body) {
         expect(body).to.equal('[0, 255. 0]')
-        done()
+        
       })
     })
   })
